c5a9ec32c1ab9fbfdfcf716ac4163215de7bcb54
Redomeire <redomeire2@gmail.com>
2022-09-04 18:00:22 +0700

    making dashboard responsive

--- src/Components/Dashboard/PartnerContainer.jsx
+++ src/Components/Dashboard/PartnerContainer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import CardPartner from "./CardPartner";
 import { AnimatePresence, motion } from "framer-motion";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 const partners = [
@@ -39,23 +38,42 @@ const partners = [
     },
 ];
 
-const marqueeVariants = {
-    animate: {
-      y: [0, -800],
-      transition: {
-        y: {
-          repeat: Infinity,
-          repeatType: "loop",
-          duration: 30,
-          ease: "linear",
-        },
-      },
+const marqueeMediumVariants = {
+    hidden: {
+        y: 0
     },
-  };
+    animate: {
+        y: [100, -800],
+        transition: {
+            y: {
+                repeat: Infinity,
+                repeatType: "loop",
+                duration: 30,
+                ease: "linear",
+            },
+        },
+    },
+};
+
+const marqueeSmallVariants = {
+    hidden: {
+        x: 0
+    },
+    animate: {
+        x: [100, -800],
+        transition: {
+            x: {
+                repeat: Infinity,
+                repeatType: "loop",
+                duration: 30,
+                ease: "linear",
+            },
+        },
+    },
+};
 
 export default function PartnerContainer() {
     const [datas, setDatas] = React.useState([]);
-    const childRef = React.useRef(null);
 
     React.useEffect(() => {
         let newPartners = partners;
@@ -64,29 +82,64 @@ export default function PartnerContainer() {
     }, [])
 
     return (
-        <div className="bg-orange rounded-xl p-8 flex justify-between overflow-hidden items-center h-[400px] shadow-2xl">
-            <div className="content w-[55%] text-white">
+        <div className="bg-orange rounded-xl p-8 flex lg:flex-row flex-col justify-between overflow-hidden items-center lg:h-[400px] shadow-2xl">
+            <div className="content lg:w-[55%] text-white">
                 <h1 className="text-3xl font-bold mb-5">Mitra Donatur</h1>
                 <p className="text-justify">Yayasan Karya Salemba Empat (KSE) yang dimulai menberikan beasiswa kepada 3 orang mahasiswa di Tahun 1995 oleh para Founder saja. Seiring dengan perkembangan masa, dari Tahun ke Tahun KSE sejak diberdirikan sebagai Yayasan di Tahun 1998, KSE mulai diberikan support oleh para donatur baik itu koorporasi dan Individual selain para pengurus dan Founder KSE.</p>
             </div>
-            <div className="w-[70%] relative">
+            <div className="md:w-[70%] relative md:-mt-0">
                 <AnimatePresence>
-                <motion.div
-                variants={marqueeVariants}
-                animate='animate'
-                transition='transition'
-                className="flex justify-end flex-wrap -mr-12">
-                    {
-                        datas.map((partner, index) => {
-                            let condition = partner.id % 4 === 0;
-                            return (
-                                <div ref={childRef}>
-                                    <CardPartner key={index} name={partner.name + ' ' + partner.id} className={condition ? '-mr-10' : ''} />
-                                </div>
-                            );
-                        })
-                    }
-                </motion.div>
+                    <motion.div
+                        variants={marqueeMediumVariants}
+                        animate='animate'
+                        transition='transition'
+                        className="lg:flex justify-end md:flex-wrap -mr-12 hidden">
+                        {
+                            datas.map((partner, index) => {
+                                let condition = partner.id % 4 === 0;
+                                return (
+                                    <div key={index}>
+                                        <CardPartner name={partner.name + ' ' + partner.id} className={condition ? 'md:-mr-10' : ''} />
+                                    </div>
+                                );
+                            })
+                        }
+                    </motion.div>
+                    <motion.div
+                        variants={marqueeSmallVariants}
+                        animate='animate'
+                        transition='transition'
+                        className="responsived flex flex-col md:flex-wrap -mr-12 mt-5 lg:hidden">
+                        <div className="flex -ml-10">
+                            {
+                                datas.map((partner, index) => {
+                                    let part = '';
+                                    if (partner.id <= partners.length / 2) {
+                                        part = <div key={index}>
+                                            <CardPartner name={partner.name + ' ' + partner.id} className='' />
+                                        </div>
+                                    }
+
+                                    return part
+                                })
+                            }
+                        </div>
+                        <div className="flex">
+                            {
+                                datas.map((partner, index) => {
+                                    let part = '';
+
+                                    if (partner.id > partners.length / 2) {
+                                        part = <div key={index}>
+                                            <CardPartner name={partner.name + ' ' + partner.id} className='' />
+                                        </div>
+                                    }
+
+                                    return part
+                                })
+                            }
+                        </div>
+                    </motion.div>
                 </AnimatePresence>
             </div>
         </div>
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -50,9 +50,9 @@ export default function Welcome() {
             <Nav />
             <div className="jumbotron bg-purple md:px-[100px] px-[50px] font-sans text-white p-32 md:pt-40 relative overflow-hidden min-h-screen">
                 <div className="mt-[85px]">
-                    <h1 className="text-5xl font-bold relative z-20">Karya Salemba Empat</h1>
-                    <h2 className="text-3xl md:font-bold mt-3 relative z-20">Universitas Brawijaya</h2>
-                    <h2 className="text-3xl md:font-bold mb-5 relative z-20">2022 - 2023</h2>
+                    <h1 className="sm:text-5xl text-3xl font-bold relative z-20">Karya Salemba Empat</h1>
+                    <h2 className="sm:text-3xl text-xl  mt-3 relative z-20">Universitas Brawijaya</h2>
+                    <h2 className="sm:text-3xl text-xl  mb-5 relative z-20">2022 - 2023</h2>
                     <a href='#berita-terkini'>
                         <button className="bg-orange shadow-lg p-2 rounded-lg font-medium px-5 py-2 text-xl  relative z-20 hover:bg-[#ff8519] text-white hover:boder-white transition hover:scale-105">Berita terkini</button>
                     </a>
@@ -60,18 +60,18 @@ export default function Welcome() {
                 <img src={JumbotronBg} alt='background' className="absolute left-0 top-32" />
             </div>
             <div className="news-card">
-                <div className="flex items-center w-[75%] justify-between mx-auto mt-20">
-                    <h3 className="w-[35%] text-2xl font-bold -mt-1">Berita Terkini</h3>
-                    <hr className="border-[1.6px] bg-black border-black rounded w-[100%] -ml-10 " />
-                    <Link to='/berita' className="w-[30%]">
+                <div className="flex md:flex-row flex-col items-center md:w-[75%] w-[80%] justify-between mx-auto mt-20">
+                    <h3 className="md:w-[35%] text-2xl font-bold md:-mt-1 mb-5 md:mb-0">Berita Terkini</h3>
+                    <hr className="border-[1.6px] bg-black border-black rounded w-[100%] md:-ml-10 mb-5 md:mb-0" />
+                    <Link to='/berita' className="md:w-[30%] ">
                         <button className="bg-black rounded-lg px-5 py-1 text-white text-sm flex items-center justify-center font-sans">
                             Lihat semua
                         </button>
                     </Link>
                 </div>
-                <div className="flex items-start mx-auto justify-center mt-10" id="berita-terkini">
+                <div className="flex items-start mx-auto justify-center mt-10 md:flex-row flex-col" id="berita-terkini">
                     <NewsCard />
-                    <div className="bg-purple rounded-xl text-white p-10 pb-7 w-2/5 ml-5 font-semibold font-sans">
+                    <div className="bg-purple rounded-xl text-white p-10 pb-7 w-[80%] mx-auto md:mx-0 md:w-2/5 md:ml-5 mt-5 md:mt-0 font-semibold font-sans">
                         <div className="mb-7">
                             <Link to='/berita' className="hover:text-gray transition">Penerimaan beswan KSE th. ajaran 2022/2023</Link>
                         </div>
@@ -83,19 +83,19 @@ export default function Welcome() {
                         </div>
                     </div>
                 </div>
-                <div className="flex w-[80%] mx-auto mt-32 items-start">
-                    <div className="tentang w-[50%]">
-                        <h1 className="font-bold text-3xl mb-2">Tentang</h1>
-                        <p className="text-justify">Berkat dukungan semua pihak, Beasiswa KSE telah berkembang dengan memberikan dukungan kepada lebih dari 20.000 mahasiswa dari 34 Perguruan Tinggi Negeri. KSE adalah jembatan bagi berbagai perusahaan dan individu dalam menyalurkan sumbangan mereka kepada kandidat dan program-program peningkatan soft skill yang tepat. Selain itu, KSE juga mendorong dan turut mempersiapkan penerima beasiswa menjadi lulusan yang memiliki integritas, berwawasan kebangsaan, cinta pada tanah air nusa dan bangsa melalui berbagai program-program pembinaan.</p>
+                <div className="flex w-[80%] mx-auto mt-32 items-start md:flex-row flex-col">
+                    <div className="tentang md:w-[50%]">
+                        <h1 className="font-bold text-3xl md:mb-2 mb-5">Tentang</h1>
+                        <p className="text-justify mb-5 md:mb-0">Berkat dukungan semua pihak, Beasiswa KSE telah berkembang dengan memberikan dukungan kepada lebih dari 20.000 mahasiswa dari 34 Perguruan Tinggi Negeri. KSE adalah jembatan bagi berbagai perusahaan dan individu dalam menyalurkan sumbangan mereka kepada kandidat dan program-program peningkatan soft skill yang tepat. Selain itu, KSE juga mendorong dan turut mempersiapkan penerima beasiswa menjadi lulusan yang memiliki integritas, berwawasan kebangsaan, cinta pada tanah air nusa dan bangsa melalui berbagai program-program pembinaan.</p>
                     </div>
-                    <div className="w-[50%] ml-20 text-justify">
+                    <div className="md:w-[50%] md:ml-20 text-justify">
                         <div>
                             <h5 className="font-bold text-xl my-2">Visi</h5>
                             <p>Mewujudkan Paguyuban KSE UB sebagai inisatior mahasiswa dalam arah gerak kolaborasi dan slaing  bersinergi untuk menciptakan inspirasi dalam memberikan nilai daya bagi KSE, Brawijaya dan Indonesia.</p>
                         </div>
                         <div>
                             <h5 className="font-bold text-xl my-2 mt-10">Misi</h5>
-                            <ol className="list-decimal ml-10">
+                            <ol className="list-decimal md:ml-10 ml-3">
                                 <li >KSE UB sebagai inspirator dalam kolaborasi dan bersinergi baik secara internal maupun secara eksternal.</li>
                                 <li>Responsif terhadap isu masalah dan hadir sebagai garda penyelesaian.</li>
                                 <li>Berperan aktif bersama demi menciptakan sumber daya yang optimal.</li>
@@ -107,12 +107,12 @@ export default function Welcome() {
                 <div className="w-[80%] mx-auto mt-32">
                     <PartnerContainer />
                 </div>
-                <div className="w-[80%] mx-auto mt-32 flex items-center">
-                    <div className="tentang w-[40%]">
-                        <h1 className="font-bold text-3xl mb-2">Apa Kata Para <br /> Beswan KSE</h1>
-                        <p className="mt-5">Testimoni Beswan KSE UB</p>
+                <div className="md:w-[80%] w-[90%] mx-auto mt-32 flex md:flex-row flex-col sm:items-start items-center">
+                    <div className="tentang md:w-[40%] md:ml-0 sm:text-left sm:ml-10 text-center">
+                        <h1 className="font-bold sm:text-3xl text-2xl mb-2">Apa Kata Para <br className="md:block hidden"/> Beswan KSE</h1>
+                        <p className="md:mt-5 mt-2">Testimoni Beswan KSE UB</p>
                     </div>
-                    <div className="tentang w-[60%]">
+                    <div className="tentang md:w-[60%] w-[90%] mx-auto md:mt-0 mt-10">
                         <Swiper className="mySwiper"
                             slidesPerView={1}
                             onInit={(e) => {
